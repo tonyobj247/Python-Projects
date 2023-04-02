916213c4250e0d336b4570fa8a49afcb9e276921
Tony Obijeih <67270462+tonyobj247@users.noreply.github.com>
2023-04-02 17:01:51 +0100

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+# String concatenation (joining different strings together using +)
+# Fill in the blanks.... "Subscribe to ________" # let's fill in the blank space with YouTube channel)
+youtube_channel = "Tony Obijeih"
+
+print("Subscribe to " + youtube_channel)
+print("Subscribe to {}" . format(youtube_channel))
+print(f"Subscribe to {youtube_channel}")
